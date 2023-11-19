61fd9dacb5d84016a4b8c4c1972a65e4c968283b
kkaszkiel <karolkaszkiel@gmail.com>
2023-11-19 19:51:00 +0100

    add django unit tests

--- movies_api/movies/tests.py
+++ movies_api/movies/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Movie
+
+class MoviesAPITestCase(TestCase):
+
+ 
+
+    def setUp(self):
+        # Create some sample movies for testing
+        self.movie1 = Movie.objects.create(title='My film 1',director='Joe Doe',year=2020)
+        self.movie2 = Movie.objects.create(title='My film 2',director='Doe Joe',year=2021)
+
+    def test_movies_list_view(self):
+        url = reverse('movie-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['content-type'], 'application/json')
+
+        data = response.json()
+        self.assertEqual(len(data), 2)
