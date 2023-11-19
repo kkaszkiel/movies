7215b1ee904a25ef55bc53c5e11f25e09ec9532a
kkaszkiel <karolkaszkiel@gmail.com>
2023-11-19 20:29:21 +0100

    .

--- movies_api/movies_api/settings.py
+++ movies_api/movies_api/settings.py
@@ -110,9 +110,9 @@ if 'test' in sys.argv or 'test_coverage' in sys.argv:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1', 
         'PORT': '5432'
     }
